7d48db17a77d055c48769860594b11b31b1b36ff
Roi Moshe <rmoshe@apple.com>
2020-04-26 21:28:26 +0300

    cont

--- exec.c
+++ exec.c
@@ -100,8 +100,8 @@ exec(char *path, char **argv)
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
   for (int i = 0; i<32; i++){
-    if( curproc->signal_handlers[i].sa_handler != SIG_IGN ){
-      curproc->signal_handlers[i].sa_handler = SIG_DFL;
+    if( curproc->signal_handlers[i].sa_handler != SIGIGN ){
+      curproc->signal_handlers[i].sa_handler = SIGDFL;
     }
   }
   
