3f7d651eacef3180a26d2a870167b1aef1598a45
Roi Moshe <rmoshe@apple.com>
2020-05-02 17:33:00 +0300

    more fixes

--- test_signal.c
+++ test_signal.c
@@ -6,7 +6,7 @@ int
 main(int argc, char *argv[])
 {
   int pid = fork();
-  int number_of_iterations = 100000000;
+  int number_of_iterations = 1000;
   if (pid == 0){ //child
     for(int i = 0; i < number_of_iterations; i++){
       printf(1, "child: %d\n", i);
