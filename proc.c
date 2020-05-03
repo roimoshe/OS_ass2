7a7ef9af5834b6176db1723cc3609cb5b756664a
Roi Moshe <rmoshe@apple.com>
2020-05-03 23:32:28 +0300

    more fixes

--- proc.c
+++ proc.c
@@ -595,12 +595,10 @@ void handle_user_level_signals(int signum){
   ///struct context context_for_user_space_sig_handler; //should be in alloc proc
   //2.4:
   memmove((void *)p->user_tf_backup, (void *)p->tf, sizeof(struct trapframe));
-  ///uint pre_eip = p->tf->eip;
   p->tf->eip = (uint)p->signal_handlers[signum].sa_handler;
   p->tf->esp -= 20;//need to push 
   memmove((void *)p->tf->esp, sigret_func, 20);
   uint *sigret_add = (uint *)p->tf->esp;
-  cprintf("sigret_add: %x", sigret_add);
   p->tf->esp -= 4;
   *(uint *)p->tf->esp = (uint)signum;
       
--- test_sig_handler.c
+++ test_sig_handler.c
@@ -8,6 +8,7 @@ volatile int keep_running = 1;
 void
 callback_for_SIG_TEST(int signum){
   keep_running = 0;
+  sigret();
 }
 
 int
