200c8c3f935aca5f126bfa831fb4d0dfb1d77dc7
Roi Moshe <rmoshe@apple.com>
2020-05-10 17:58:48 +0300

    cont

--- proc.c
+++ proc.c
@@ -340,6 +340,8 @@ wait(void)
         pid = p->pid;
         kfree(p->kstack);
         p->kstack = 0;
+        kfree(p->user_tf_backup);
+        p->user_tf_backup = 0;
         freevm(p->pgdir);
         p->pid = 0;
         p->parent = 0;
