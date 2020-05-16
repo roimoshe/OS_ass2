9570b6463e8a26970596c701c7af0d5d12a86a94
roi moshe <roimoshe@github.com>
2020-05-16 19:00:37 +0300

    add tests.c

---
+++ tests.c
@@ -0,0 +1,51 @@
+// command for shell: test_signal
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  for (int i = 0; i<10; i++){
+    int number_of_iterations = 50;
+    int pid = fork();
+    if (pid == 0){ //child
+      for(int i = 0; 1; i++){
+        printf(1, "child: %d\n", i);
+      }
+      exit();
+    } else{ //parent
+      for(int i = 0; i < number_of_iterations; i++){
+        printf(1, "parent: %d\n", i);
+      }
+      printf(1, "send SIGSTOP to child ");
+      if(kill(pid, SIGSTOP) == 0){
+        printf(1, "ACK\n");
+      } else{
+        printf(1, "NACK\n");
+      }
+      for(int i = number_of_iterations; i < 2*number_of_iterations; i++){
+        printf(1, "parent: %d\n", i);
+      }
+      printf(1, "send SIGCONT to child ");
+      if(kill(pid, SIGCONT) == 0){
+        printf(1, "ACK\n");
+      } else{
+        printf(1, "NACK\n");
+      }
+      for(int i = 2*number_of_iterations; i < 3*number_of_iterations; i++){
+        printf(1, "parent: %d\n", i);
+      }
+      printf(1, "send SIGKILL to child and wait till he exits ");
+      if(kill(pid, SIGKILL) == 0){
+        printf(1, "ACK\n");
+      } else{
+        printf(1, "NACK\n");
+      }
+      wait();
+      printf(1, ">>>SIGSTOP/SIGCONT PASS<<<\n");
+    }
+  }
+
+  exit();
+}
