07449781ba3994e9d9375703ee8079802dd5b0e7
Roi Moshe <rmoshe@apple.com>
2020-05-16 15:09:30 +0300

    test_sinal_loop_new_branch

--- test_signal.c
+++ test_signal.c
@@ -6,51 +6,52 @@
 int
 main(int argc, char *argv[])
 {
-  printf(1, ">>>TEST SIGKILL<<<\n");
-  int pid = fork();
-  int number_of_iterations = 100;
-  if (pid == 0){ //child
-    for(int i = 0; i < number_of_iterations; i++){
-      printf(1, "child: %d\n", i);
+  for (int i = 0; i<10; i++){
+    printf(1, ">>>TEST SIGKILL<<<\n");
+    int pid = fork();
+    int number_of_iterations = 20;
+    if (pid == 0){ //child
+      for(int i = 0; i < number_of_iterations; i++){
+        printf(1, "child: %d\n", i);
+      }
+      exit();
+    } else{ //parent
+      for(int i = 0; i < number_of_iterations/2; i++){
+        printf(1, "parent: %d\n", i);
+      }
+      printf(1, "send SIGKILL to child and wait till he exits\n");
+      kill(pid, SIGKILL);
+      wait();
+      printf(1, ">>>SIGKILL PASS<<<\n");
     }
-    exit();
-  } else{ //parent
-    for(int i = 0; i < number_of_iterations/2; i++){
-      printf(1, "parent: %d\n", i);
+
+    printf(1, ">>>TEST SIGSTOP/SIGCONT<<<\n");
+    pid = fork();
+    if (pid == 0){ //child
+      for(int i = 0; i < number_of_iterations*100; i++){
+        printf(1, "child: %d\n", i);
+      }
+      exit();
+    } else{ //parent
+      for(int i = 0; i < number_of_iterations; i++){
+        printf(1, "parent: %d\n", i);
+      }
+      printf(1, "send SIGSTOP to child\n");
+      kill(pid, SIGSTOP);
+      for(int i = number_of_iterations; i < 2*number_of_iterations; i++){
+        printf(1, "parent: %d\n", i);
+      }
+      printf(1, "send SIGCONT to child\n");
+      kill(pid, SIGCONT);
+      for(int i = 2*number_of_iterations; i < 3*number_of_iterations; i++){
+        printf(1, "parent: %d\n", i);
+      }
+      printf(1, "send SIGKILL to child and wait till he exits\n");
+      kill(pid, SIGKILL);
+      wait();
+      printf(1, ">>>SIGSTOP/SIGCONT PASS<<<\n");
     }
-    printf(1, "send SIGKILL to child and wait till he exits\n");
-    kill(pid, SIGKILL);
-    wait();
-    printf(1, ">>>SIGKILL PASS<<<\n");
   }
-
-  printf(1, ">>>TEST SIGSTOP/SIGCONT<<<\n");
-  pid = fork();
-  if (pid == 0){ //child
-    for(int i = 0; i < number_of_iterations*100; i++){
-      printf(1, "child: %d\n", i);
-    }
-    exit();
-  } else{ //parent
-    for(int i = 0; i < number_of_iterations; i++){
-      printf(1, "parent: %d\n", i);
-    }
-    printf(1, "send SIGSTOP to child\n");
-    kill(pid, SIGSTOP);
-    for(int i = number_of_iterations; i < 2*number_of_iterations; i++){
-      printf(1, "parent: %d\n", i);
-    }
-    printf(1, "send SIGCONT to child\n");
-    kill(pid, SIGCONT);
-    for(int i = 2*number_of_iterations; i < 3*number_of_iterations; i++){
-      printf(1, "parent: %d\n", i);
-    }
-    printf(1, "send SIGKILL to child and wait till he exits\n");
-    kill(pid, SIGKILL);
-    wait();
-    printf(1, ">>>SIGSTOP/SIGCONT PASS<<<\n");
-  }
-
-
+  printf(1, ">>>!!!PASS ALL!!!<<<\n");
   exit();
 }
