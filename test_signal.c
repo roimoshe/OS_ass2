a51abaf377eca6f0977992274bdd16d241a51cfa
Roy <robd7777@gmail.com>
2020-05-19 20:13:36 +0300

    refactor test_signal

--- test_signal.c
+++ test_signal.c
@@ -6,42 +6,49 @@
 int
 main(int argc, char *argv[])
 {
-  for (int i = 0; i<10; i++){
-    int number_of_iterations = 50;
+  int num_of_loops = 1
+  for (int i = 0; i<num_of_loops; i++){
+
     int pid = fork();
+    
     if (pid == 0){ //child
-      for(int i = 0; 1; i++){
-        printf(1, "child: %d\n", i);
+      while(1){
+        printf(1, "child still not dead\n");
       }
       exit();
-    } else{ //parent
-      for(int i = 0; i < number_of_iterations; i++){
-        printf(1, "parent: %d\n", i);
-      }
+    } 
+    else{ //parent
+      sleep(5);
+    
       printf(1, "send SIGSTOP to child ");
       if(kill(pid, SIGSTOP) == 0){
         printf(1, "ACK\n");
       } else{
         printf(1, "NACK\n");
       }
-      for(int i = number_of_iterations; i < 2*number_of_iterations; i++){
-        printf(1, "parent: %d\n", i);
-      }
+      sleep(5);
+
       printf(1, "send SIGCONT to child ");
       if(kill(pid, SIGCONT) == 0){
         printf(1, "ACK\n");
       } else{
         printf(1, "NACK\n");
       }
-      for(int i = 2*number_of_iterations; i < 4*number_of_iterations; i++){
-        printf(1, "parent: %d\n", i);
+
+      printf(1, "send onther SIGCONT to child  (unused)");
+      if(kill(pid, SIGCONT) == 0){
+        printf(1, "ACK\n");
+      } else{
+        printf(1, "NACK\n");
       }
+
       printf(1, "send SIGKILL to child and wait till he exits ");
       if(kill(pid, SIGKILL) == 0){
         printf(1, "ACK\n");
       } else{
         printf(1, "NACK\n");
       }
+
       wait();
       printf(1, ">>>SIGSTOP/SIGCONT PASS<<<\n");
     }
